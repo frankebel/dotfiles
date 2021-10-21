eecc4211517d129a3c57b976f7eb8ee9bf13c6e6
frankebel <git@ebel.page>
2021-10-21 09:52:01 +0200

    rename wmanager

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -370,4 +370,5 @@ auto_minimize = True
 #
 # We choose LG3D to maximize irony: it is a 3D non-reparenting WM written in
 # java that happens to be on java's whitelist.
-wmname = "LG3D"
+# wmname = "LG3D"
+wmname = 'Qtile'
