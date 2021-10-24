24b5d649b729f37c0390dc2023dea5c9f2ef2d9c
frankebel <git@ebel.page>
2021-10-24 13:19:36 +0200

    remove autostart applications

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -373,11 +373,6 @@ auto_fullscreen = True
 focus_on_window_activation = "smart"
 reconfigure_screens = True
 
-@hook.subscribe.startup_once
-def autostart():
-    home = os.path.expanduser('~/.config/qtile/autostart.sh')
-    subprocess.call([home])
-
 # If things like steam games want to auto-minimize themselves when losing
 # focus, should we respect this or not?
 auto_minimize = True
