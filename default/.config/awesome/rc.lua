340607de9a66911e6bd00a48d7cee60ebb89d570
frankebel <git@ebel.page>
2022-06-07 11:14:13 +0200

    Remove maximied applications.

--- default/.config/awesome/rc.lua
+++ default/.config/awesome/rc.lua
@@ -423,16 +423,6 @@ awful.rules.rules = {
       properties = { fullscreen = true }
     },
 
-
-    -- Maximizied applications.
-    { rule_any = {
-        class = {
-	  "Sxiv",
-        }
-      },
-      properties = { maximized = true }
-    },
-
     -- Set Firefox to always map on the tag named "2" on screen 1.
     -- { rule = { class = "Firefox" },
     --   properties = { screen = 1, tag = "2" } },
