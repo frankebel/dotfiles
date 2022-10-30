a6a204e658d3d4412a5e7639fd5312beb8f0d19e
frankebel <git@ebel.page>
2022-10-30 20:28:00 +0100

    feat: Install all for treesitter

--- home/.config/nvim/after/plugin/treesitter.lua
+++ home/.config/nvim/after/plugin/treesitter.lua
@@ -3,15 +3,7 @@
 
 require "nvim-treesitter.configs".setup {
   -- A list of parser names, or "all"
-  ensure_installed = {
-    "bash",
-    "c",
-    "java",
-    "latex",
-    "lua",
-    "markdown",
-    "python",
-  },
+  ensure_installed = "all",
   sync_install = false,
   auto_install = true,
   highlight = {
