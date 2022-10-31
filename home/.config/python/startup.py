7715e260cbae4840a9a4a6f4c7d81c0a861ca45c
frankebel <git@ebel.page>
2022-10-31 17:46:32 +0100

    feat: move python history file location
    Create history file at `$XDG_STATE_HOME/python/history` instead of
    `$HOME/.python_history`.

--- home/.config/python/startup.py
+++ home/.config/python/startup.py
@@ -3,12 +3,19 @@ import atexit
 import os
 import readline
 
-histfile = os.path.join(os.environ['XDG_DATA_HOME'], 'python/python_history')
+histfile = os.path.join(os.environ["XDG_STATE_HOME"], "python/history")
+histpath = os.path.split(histfile)[0]
+
+if not os.path.exists(histpath):
+    os.makedirs(histpath)
 try:
     readline.read_history_file(histfile)
-    # default history len is -1 (infinite), which may grow unruly
-    readline.set_history_length(1000)
 except FileNotFoundError:
     pass
+# Default history len is -1 (infinite), which may grow unruly.
+readline.set_history_length(1000)
+# Prevent creation of default history if custom is empty.
+if readline.get_current_history_length() == 0:
+    readline.add_history("# dummy text")
 
 atexit.register(readline.write_history_file, histfile)
