b5f23c44621e00b0a4f760907ab80615648eb3b7
frankebel <git@ebel.page>
2022-11-15 20:56:55 +0100

    fix: fix diagnostics

--- home/.config/nvim/after/plugin/lsp.lua
+++ home/.config/nvim/after/plugin/lsp.lua
@@ -36,7 +36,7 @@ vim.keymap.set("n", "<leader>dq", vim.diagnostic.setloclist)
 
 -- Use an on_attach function to only map the following keys
 -- after the language server attaches to the current buffer
-local on_attach = function(client, bufnr)
+local on_attach = function(_, bufnr)
   -- Enable completion triggered by <c-x><c-o>
   vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
 
