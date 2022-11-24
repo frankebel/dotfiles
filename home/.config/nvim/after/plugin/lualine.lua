7e0480c9690572c67a6006bfe6c5baa0dc986493
Frank Ebel <git@ebel.page>
2022-11-24 19:25:50 +0100

    feat: set filename path to "Relative path"

--- home/.config/nvim/after/plugin/lualine.lua
+++ home/.config/nvim/after/plugin/lualine.lua
@@ -7,6 +7,20 @@ require "lualine".setup {
     section_separators = "",
   },
   sections = {
+    lualine_c = {
+      {
+        "filename",
+        path = 1,
+      },
+    },
     lualine_x = { "filetype" },
+  },
+  inactive_sections = {
+    lualine_c = {
+      {
+        "filename",
+        path = 1,
+      },
+    },
   },
 }
