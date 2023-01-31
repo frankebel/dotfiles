bb648511a210439a05cc54617985311a39b4cf48
Frank Ebel <git@ebel.page>
2023-01-31 19:42:23 +0100

    feat(neovim): remove TSUpdate error
    Remove error when installing nvim-treesitter for the first time.
    https://github.com/nvim-treesitter/nvim-treesitter/wiki/Installation#packernvim

--- home/.config/nvim/lua/user/plugins.lua
+++ home/.config/nvim/lua/user/plugins.lua
@@ -66,7 +66,10 @@ return require("packer").startup(function(use)
   -- Treesitter
   use {
     "nvim-treesitter/nvim-treesitter",
-    run = ":TSUpdate",
+    run = function()
+      local ts_update = require("nvim-treesitter.install").update({ with_sync = true })
+      ts_update()
+    end,
   }
   use "nvim-treesitter/nvim-treesitter-context"
   use {
