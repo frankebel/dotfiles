7fb9c3ac3fa8a8f6b06198c7bbf70ec66131f7ec
Frank Ebel <git@ebel.page>
2023-02-02 15:26:46 +0100

    feat(neovim): update sumneko lua lsp

--- home/.config/nvim/after/plugin/lsp.lua
+++ home/.config/nvim/after/plugin/lsp.lua
@@ -25,6 +25,10 @@ lspconfig["sumneko_lua"].setup {
       workspace = {
         -- Make the server aware of Neovim runtime files
         library = vim.api.nvim_get_runtime_file("", true),
+        checkThirdParty = false,
+      },
+      telemetry = {
+        enable = false,
       },
     },
   },
