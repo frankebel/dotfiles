0ce043d10f1e5762a141065fb54a5e45c7f4c722
Frank Ebel <git@ebel.page>
2023-02-27 22:11:15 +0100

    feat(neovim): update keymaps

--- home/.config/nvim/lua/user/keymaps.lua
+++ home/.config/nvim/lua/user/keymaps.lua
@@ -9,7 +9,7 @@ keymap("n", "<C-h>", "<C-w>h", { silent = true })
 keymap("n", "<C-j>", "<C-w>j", { silent = true })
 keymap("n", "<C-k>", "<C-w>k", { silent = true })
 keymap("n", "<C-l>", "<C-w>l", { silent = true })
-keymap("n", "<leader>v", "<cmd>vsplit<cr>", { desc = "vsplit"})
+keymap("n", "<c-v>", "<cmd>vsplit<cr>", { desc = "vsplit" })
 
 -- Indent mode stay
 keymap("v", "<", "<gv", { silent = true })
@@ -19,14 +19,11 @@ keymap("v", ">", ">gv", { silent = true })
 keymap("x", "J", ":move '>+1<CR>gv-gv", { silent = true })
 keymap("x", "K", ":move '<-2<CR>gv-gv", { silent = true })
 
--- Delete without yanking
-keymap("n", "<leader>dd", '"_dd', { desc = "Delete without yanking" })
-keymap("v", "<leader>d", '"_d', { desc = "Delete without yanking" })
 -- Paste without yanking
 keymap("v", "p", '"_dP', { desc = "Paste without yanking" })
 
 -- Close current buffer
-keymap("n", "<leader>bd", "<cmd>bn<cr><cmd>bd#<cr>", { desc = "Delete current buffer"})
+keymap("n", "<leader>bd", "<cmd>bn<cr><cmd>bd#<cr>", { desc = "Delete current buffer" })
 
 -- Toggle options
 -- Start with "<leader>o"
