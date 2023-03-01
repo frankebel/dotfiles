f81d4240af25988168ac1ed22091d5f71a3240fd
Frank Ebel <git@ebel.page>
2023-03-01 12:46:09 +0100

    feat(neovim): configure completion to lazy

---
+++ home/.config/nvim/lua/plugins/completion.lua
@@ -0,0 +1,71 @@
+-- Completion engine
+-- https://github.com/hrsh7th/nvim-cmp
+
+return {
+  "hrsh7th/nvim-cmp",
+  dependencies = {
+    -- luasnip
+    { "L3MON4D3/LuaSnip", version = "*" },
+    "saadparwaiz1/cmp_luasnip",
+    -- completion plugins
+    "hrsh7th/cmp-nvim-lsp",
+    "hrsh7th/cmp-nvim-lua",
+    "hrsh7th/cmp-buffer",
+    "hrsh7th/cmp-path",
+    -- pictograms
+    "onsails/lspkind.nvim",
+  },
+  config = function()
+    local cmp = require("cmp")
+
+    cmp.setup {
+      snippet = {
+        expand = function(args)
+          require "luasnip".lsp_expand(args.body)
+        end,
+      },
+
+      window = {
+        -- completion = cmp.config.window.bordered(),
+        -- documentation = cmp.config.window.bordered(),
+      },
+
+      mapping = {
+        -- ":help ins-completion"
+        ["<c-p>"] = cmp.mapping.select_prev_item(),
+        ["<c-n>"] = cmp.mapping.select_next_item(),
+        ["<c-b>"] = cmp.mapping.scroll_docs( -4),
+        ["<c-f>"] = cmp.mapping.scroll_docs(4),
+        ["<c-c>"] = cmp.mapping.complete(),
+        ["<c-e>"] = cmp.mapping.abort(),
+        ["<c-y>"] = cmp.mapping.confirm({ select = true }),
+      },
+
+      formatting = {
+        format = require("lspkind").cmp_format {
+          with_text = true,
+          menu = {
+            nvim_lsp = "[LSP]",
+            nvim_lua = "[LUA]",
+            luasnip = "[SNIP]",
+            buffer = "[BUF]",
+            path = "[PATH]",
+          },
+        },
+      },
+
+      sources = {
+        { name = "nvim_lsp" },
+        { name = "nvim_lua" },
+        { name = "luasnip" },
+        { name = "buffer" },
+        { name = "path" },
+      },
+
+      confirm_opts = {
+        behavior = cmp.ConfirmBehavior.Replace,
+        select = false,
+      },
+    }
+  end,
+}
