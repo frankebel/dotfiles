fbb01c5a1fa904dd8a7a2878129717469f890a78
Frank Ebel <git@ebel.page>
2023-03-03 13:56:04 +0100

    refactor(neovim): move keymap to plugin config

--- home/.config/nvim/after/ftplugin/markdown.lua
+++ home/.config/nvim/after/ftplugin/markdown.lua
@@ -2,6 +2,3 @@
 vim.opt.shiftwidth = 2
 vim.opt.softtabstop = 2
 vim.opt.tabstop = 2
-
--- Keymaps
-vim.keymap.set("n", "<leader>md", "<cmd>MarkdownPreviewToggle<cr>", { desc = "Toggle markdown preview" })
--- home/.config/nvim/lua/plugins/ft.lua
+++ home/.config/nvim/lua/plugins/ft.lua
@@ -11,5 +11,8 @@ return {
     "iamcco/markdown-preview.nvim",
     build = "cd app && yarn install",
     ft = "markdown",
+    config = function()
+      vim.keymap.set("n", "<leader>md", "<cmd>MarkdownPreviewToggle<cr>", { desc = "Toggle markdown preview" })
+    end,
   }
 }
