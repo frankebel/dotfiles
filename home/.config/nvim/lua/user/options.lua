355490b0876c296c8f74c3c36947e6ba4bcda149
Frank Ebel <git@ebel.page>
2023-03-04 19:17:25 +0100

    feat(neovim): update options

--- home/.config/nvim/lua/user/options.lua
+++ home/.config/nvim/lua/user/options.lua
@@ -1,59 +1,36 @@
 -- Options
--- :help options
--- https://neovim.io/doc/user/options.html
 
--- Leader key
+-- Special Characters
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
--- Formatoptions
-vim.opt.formatoptions:remove("a")
-vim.opt.formatoptions:remove("o")
-vim.opt.formatoptions:remove("r")
-vim.opt.formatoptions:remove("t")
-vim.opt.formatoptions:append("n")
-vim.opt.formatoptions:append("c")
-
--- Line number
-vim.opt.number = true
-vim.opt.numberwidth = 2
-vim.opt.relativenumber = true
-
--- Line wrap
-vim.opt.linebreak = true
-vim.opt.showbreak = "   "
-vim.opt.wrap = true
-
--- Scroll
-vim.opt.scrolloff = 8
-vim.opt.sidescrolloff = 8
-
--- Search
-vim.opt.hlsearch = true
-vim.opt.ignorecase = true
-vim.opt.incsearch = true
-vim.opt.smartcase = true
-
--- Split
--- vim.opt.splitbelow = true
--- vim.opt.splitright = true
-
--- Tabs and indents
-vim.opt.autoindent = true
-vim.opt.expandtab = true
-vim.opt.shiftwidth = 4
-vim.opt.smarttab = true
-vim.opt.softtabstop = 4
-vim.opt.tabstop = 4
-
--- Uncategorized
-vim.opt.clipboard = "unnamedplus"
-vim.opt.cmdheight = 1
-vim.opt.colorcolumn = "80"
-vim.opt.completeopt = { "menuone", "noselect" }
-vim.opt.cursorline = true
-vim.opt.mouse = "a"
-vim.opt.showmode = false
-vim.opt.signcolumn = "yes"
-vim.opt.termguicolors = true
-vim.opt.undofile = true
+-- `:help options`
+-- Compact list under `:help option-list`
+vim.opt.clipboard = "unnamedplus"               -- Use system clipboard
+vim.opt.colorcolumn = "80"                      -- Screen column
+vim.opt.completeopt = { "menuone", "noselect" } -- Insert mode comlete options
+vim.opt.cursorline = true                       -- Highlight text line of cursor line
+vim.opt.expandtab = true                        -- Expand tabs into spaces
+vim.opt.formatoptions = "tcroqnj"               -- Default "tcqj"
+vim.opt.ignorecase = true                       -- Ignore case in search
+vim.opt.mouse = "a"                             -- Enable mouse support
+vim.opt.number = true                           -- Print line number
+vim.opt.relativenumber = true                   -- Show relative line number
+vim.opt.scrolloff = 8                           -- Minimal number of lines to keep around cursor
+vim.opt.shiftwidth = 4                          -- Number of spaces for indent
+vim.opt.shortmess:append({ I = true })          -- Don't give intro message when starting Vim
+vim.opt.showmode = false                        -- Don't show mode
+vim.opt.sidescrolloff = 8                       -- Minimal number of columns to keep around cursor
+vim.opt.signcolumn = "yes"                      -- Always show signcolumn
+vim.opt.smartcase = true                        -- Overwrite 'ignorecase' with upper case characters
+vim.opt.smartindent = true                      -- Smart autoindent when starting a new line
+vim.opt.softtabstop = 4                         -- Number of spaces <Tab> counts for while performing editing operations
+vim.opt.spelllang = { "en" }                    -- Spellcheck for these languages
+vim.opt.splitbelow = true                       -- Splitting window below current one
+vim.opt.splitright = true                       -- Splitting window right of current one
+vim.opt.tabstop = 4                             -- Number of spaces <Tab> counts for
+vim.opt.termguicolors = true                    -- Enable 24-bit RGB color
+vim.opt.timeoutlen = 300                        -- Time in ms to wait for mapped sequence to complete
+vim.opt.undofile = true                         -- Save undo history
+vim.opt.wildmode = "longest:full,full"          -- Completion mode
+vim.opt.wrap = false                            -- Line wrap
