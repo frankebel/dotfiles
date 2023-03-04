3243c9f673669c2d262f028a9ca917f793c60986
Frank Ebel <git@ebel.page>
2023-03-05 00:33:34 +0100

    refactor(neovim): install directly in mason

--- home/.config/nvim/lua/plugins/mason.lua
+++ home/.config/nvim/lua/plugins/mason.lua
@@ -1,31 +1,34 @@
 -- Use Mason as package manager for LSP, DAP, linters, formatters.
 -- https://github.com/williamboman/mason.nvim
--- https://github.com/WhoIsSethDaniel/mason-tool-installer.nvim
 
 return {
   {
     "williamboman/mason.nvim",
-    config = true,
-  },
-  {
-    "WhoIsSethDaniel/mason-tool-installer.nvim",
-    opts = {
-      ensure_installed = {
+    config = function()
+      local ensure_installed = {
         -- Java
-        "jdtls", -- LSP
-        "java-debug-adapter", -- DAP
-        "java-test", -- DAP
+        "jdtls",               -- LSP
+        "java-debug-adapter",  -- DAP
+        "java-test",           -- DAP
         -- Lua
         "lua-language-server", -- LSP
         -- Python
-        "pyright", -- LSP
-        "debugpy", -- DAP
-        "black", -- formatter
-        "flake8", -- linter
+        "pyright",             -- LSP
+        "debugpy",             -- DAP
+        "black",               -- formatter
+        "flake8",              -- linter
         -- Shell
-        "shellcheck", -- linter
-        "shfmt", -- formatter
-      },
-    },
+        "shellcheck",          -- linter
+        "shfmt",               -- formatter
+      }
+      require("mason").setup()
+      local mr = require("mason-registry")
+      for _, tool in ipairs(ensure_installed) do
+        local p = mr.get_package(tool)
+        if not p:is_installed() then
+          p:install()
+        end
+      end
+    end,
   },
 }
