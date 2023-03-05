175819b5f5b8b2012df1813b8f0a4ccb19d79311
Frank Ebel <git@ebel.page>
2023-03-05 13:22:18 +0100

    fix(neovim): fix treesitter selection

--- home/.config/nvim/lua/plugins/treesitter.lua
+++ home/.config/nvim/lua/plugins/treesitter.lua
@@ -80,16 +80,16 @@ return {
                   ["[F"] = { query = "@function.outer", desc = "Jump to previous function end" },
                   ["[C"] = { query = "@class.outer", desc = "Jump to previous class end" },
             },
-            select = {
-              enable = true,
-              lookahead = true,
-              keymaps = {
-                -- You can use the capture groups defined in textobjects.scm
-                    ["af"] = { query = "@function.outer", desc = "Select around function" },
-                    ["if"] = { query = "@function.inner", desc = "Select inside function" },
-                    ["ic"] = { query = "@class.inner", desc = "Select inside class" },
-                    ["ac"] = { query = "@class.outer", desc = "Select around class" },
-              },
+          },
+          select = {
+            enable = true,
+            lookahead = true,
+            keymaps = {
+              -- You can use the capture groups defined in textobjects.scm
+                  ["af"] = { query = "@function.outer", desc = "Select around function" },
+                  ["if"] = { query = "@function.inner", desc = "Select inside function" },
+                  ["ic"] = { query = "@class.inner", desc = "Select inside class" },
+                  ["ac"] = { query = "@class.outer", desc = "Select around class" },
             },
           },
           swap = {
