1074797121c091ad37b5fad6438fdcc025de7ee2
Frank Ebel <git@ebel.page>
2023-03-05 17:14:23 +0100

    perf(neovimn): lazy load nvim-dap-python

--- home/.config/nvim/lua/plugins/dap.lua
+++ home/.config/nvim/lua/plugins/dap.lua
@@ -47,6 +47,7 @@ return {
   },
   {
     "mfussenegger/nvim-dap-python",
+    ft = "python",
     config = function()
       require("dap-python").setup(vim.fn.stdpath("data") .. "/mason/packages/debugpy/venv/bin/python")
     end,
