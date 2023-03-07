e6aff1978cf8226d086450a3a888e3aceab94a7a
Frank Ebel <git@ebel.page>
2023-03-07 19:06:59 +0100

    feat(neovim): remove options for python

--- home/.config/nvim/after/ftplugin/python.lua
+++ home/.config/nvim/after/ftplugin/python.lua
@@ -1,13 +1,2 @@
--- Line wrap
-vim.opt.textwidth = 79
-
--- Tabs and indents
-vim.opt.shiftwidth = 4
-vim.opt.softtabstop = 4
-vim.opt.tabstop = 4
-
--- Uncategorized
-vim.opt.colorcolumn = "80"
-
 -- Keymaps
 vim.keymap.set("n", "<leader>x", "<cmd>!python %<cr>", { desc = "Run current file" })
