4d79ec4c403fd20272a90a2dca38067cc5b505b4
Frank Ebel <git@ebel.page>
2023-03-12 17:29:40 +0100

    feat(neovim): add plugin vim-illuminate

--- home/.config/nvim/lua/plugins/editor.lua
+++ home/.config/nvim/lua/plugins/editor.lua
@@ -32,4 +32,14 @@ return {
     event = { "BufReadPre", "BufNewFile" },
     config = true,
   },
+
+  -- highlight uses of word under cursor
+  -- https://github.com/RRethy/vim-illuminate
+  {
+    "RRethy/vim-illuminate",
+    event = { "BufReadPost", "BufNewFile" },
+    config = function()
+      require("illuminate").configure({})
+    end,
+  },
 }
