abc55f4ce1d10c658596bd0294b1e532bea4bc54
Frank Ebel <git@ebel.page>
2023-03-14 22:54:11 +0100

    refactor(awesome): remove buttons for tasklist

--- home/.config/awesome/rc.lua
+++ home/.config/awesome/rc.lua
@@ -128,25 +128,6 @@ if not is_laptop() then
   table.remove(widgets_right, 3)
 end
 
-local tasklist_buttons = gears.table.join(
-  awful.button({}, 1, function(c)
-    if c == client.focus then
-      c.minimized = true
-    else
-      c:emit_signal("request::activate", "tasklist", { raise = true })
-    end
-  end),
-  awful.button({}, 3, function()
-    awful.menu.client_list({ theme = { width = 250 } })
-  end),
-  awful.button({}, 4, function()
-    awful.client.focus.byidx(1)
-  end),
-  awful.button({}, 5, function()
-    awful.client.focus.byidx(-1)
-  end)
-)
-
 awful.screen.connect_for_each_screen(function(s)
   -- Each screen has its own tag table.
   awful.tag({ "1", "2", "3", "4", "5", "6", "7", "8", "9" }, s, awful.layout.layouts[1])
@@ -180,7 +161,6 @@ awful.screen.connect_for_each_screen(function(s)
   s.mytasklist = awful.widget.tasklist({
     screen = s,
     filter = awful.widget.tasklist.filter.currenttags,
-    buttons = tasklist_buttons,
   })
 
   -- Create the wibox
