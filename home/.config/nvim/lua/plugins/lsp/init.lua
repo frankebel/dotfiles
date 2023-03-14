638f8173ebef2bf7cd0c600e1b14fc5cddd8b3fe
Frank Ebel <git@ebel.page>
2023-03-14 23:21:57 +0100

    refactor(neovim): remove neovim runtime files
    If they are added, go to defition will show the same file twice.

--- home/.config/nvim/lua/plugins/lsp/init.lua
+++ home/.config/nvim/lua/plugins/lsp/init.lua
@@ -39,8 +39,6 @@ return {
             },
             format = { enable = false },
             workspace = {
-              -- Make the server aware of Neovim runtime files
-              library = vim.api.nvim_get_runtime_file("", true),
               checkThirdParty = false,
             },
             telemetry = { enable = false },
