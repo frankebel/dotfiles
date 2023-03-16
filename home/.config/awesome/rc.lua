f78a821195a2fe12720e8aab98a7da5177579752
Frank Ebel <git@ebel.page>
2023-03-16 21:06:33 +0100

    refactor(awesome): move signals to own file

--- home/.config/awesome/rc.lua
+++ home/.config/awesome/rc.lua
@@ -2,10 +2,8 @@
 -- Website: https://awesomewm.org/
 -- Docs: https://awesomewm.org/apidoc
 
--- Awesome libraries
-local awful = require("awful")
+-- Awesome library
 require("awful.autofocus") -- focus after closing window
-local beautiful = require("beautiful")
 
 -- Error handling
 require("handling")
@@ -25,24 +23,4 @@ require("wibar")
 
 require("rules") -- window rules
 
--- {{{ Signals
--- Signal function to execute when a new client appears.
-client.connect_signal("manage", function(c)
-  -- Set the windows at the slave,
-  -- i.e. put it at the end of others instead of setting it master.
-  -- if not awesome.startup then awful.client.setslave(c) end
-
-  if awesome.startup and not c.size_hints.user_position and not c.size_hints.program_position then
-    -- Prevent clients from being unreachable after screen count changes.
-    awful.placement.no_offscreen(c)
-  end
-end)
-
--- Mark active window with border.
-client.connect_signal("focus", function(c)
-  c.border_color = beautiful.border_focus
-end)
-client.connect_signal("unfocus", function(c)
-  c.border_color = beautiful.border_normal
-end)
--- }}}
+require("signals")
