63277849729c31c7561bd0747b36b15040b59cb1
Frank Ebel <git@ebel.page>
2023-03-16 19:53:56 +0100

    refactor(awesome): move layouts to own file

--- home/.config/awesome/rc.lua
+++ home/.config/awesome/rc.lua
@@ -15,6 +15,9 @@ require("handling")
 -- Load custom theme
 require("themes")
 
+-- List of layouts
+require("layouts")
+
 -- Function to get hostname
 local function gethostname()
   local f = assert(io.popen("uname -n", "r"))
@@ -33,26 +36,6 @@ local function is_laptop()
     return false
   end
 end
-
--- Table of layouts to cover with awful.layout.inc, order matters.
-awful.layout.layouts = {
-  -- awful.layout.suit.floating,
-  awful.layout.suit.tile,
-  -- awful.layout.suit.tile.left,
-  -- awful.layout.suit.tile.bottom,
-  -- awful.layout.suit.tile.top,
-  -- awful.layout.suit.fair,
-  -- awful.layout.suit.fair.horizontal,
-  -- awful.layout.suit.spiral,
-  -- awful.layout.suit.spiral.dwindle,
-  -- awful.layout.suit.max,
-  -- awful.layout.suit.max.fullscreen,
-  -- awful.layout.suit.magnifier,
-  -- awful.layout.suit.corner.nw,
-  -- awful.layout.suit.corner.ne,
-  -- awful.layout.suit.corner.sw,
-  -- awful.layout.suit.corner.se,
-}
 
 -- {{{ Wibar
 -- Set path for custom widgets.
