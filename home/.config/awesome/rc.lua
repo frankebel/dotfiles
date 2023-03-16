9db9c12add550e1a328aa1a2410030842341f3f3
Frank Ebel <git@ebel.page>
2023-03-16 17:39:53 +0100

    refactor(awesome): move error handling to own file

--- home/.config/awesome/rc.lua
+++ home/.config/awesome/rc.lua
@@ -14,39 +14,9 @@ require("awful.autofocus")
 local wibox = require("wibox")
 -- Theme handling library
 local beautiful = require("beautiful")
--- Notification library
-local naughty = require("naughty")
 
--- {{{ Error handling
--- Check if awesome encountered an error during startup and fell back to
--- another config (This code will only ever execute for the fallback config)
-if awesome.startup_errors then
-  naughty.notify({
-    preset = naughty.config.presets.critical,
-    title = "Oops, there were errors during startup!",
-    text = awesome.startup_errors,
-  })
-end
-
--- Handle runtime errors after startup
-do
-  local in_error = false
-  awesome.connect_signal("debug::error", function(err)
-    -- Make sure we don't go into an endless error loop
-    if in_error then
-      return
-    end
-    in_error = true
-
-    naughty.notify({
-      preset = naughty.config.presets.critical,
-      title = "Oops, an error happened!",
-      text = tostring(err),
-    })
-    in_error = false
-  end)
-end
--- }}}
+-- Error handling
+require("errors")
 
 -- Function to get hostname
 local function gethostname()
