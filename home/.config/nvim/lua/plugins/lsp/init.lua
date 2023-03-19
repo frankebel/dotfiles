bd88a54600251124342f4623b4dc10445cddf891
Frank Ebel <git@ebel.page>
2023-03-19 17:08:11 +0100

    feat(neovim): update lua_ls globals
    Add "screen" global for awesomewm.

--- home/.config/nvim/lua/plugins/lsp/init.lua
+++ home/.config/nvim/lua/plugins/lsp/init.lua
@@ -35,6 +35,7 @@ return {
                 "awesome",
                 "client",
                 "root",
+                "screen",
                 "tag",
                 -- neovim
                 "vim",
