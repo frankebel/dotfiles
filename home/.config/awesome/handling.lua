f645e3240a6e7dcf1a9404c239d5b7619f911572
Frank Ebel <git@ebel.page>
2023-03-28 14:22:17 +0200

    style(awesome): autoformat file

--- home/.config/awesome/handling.lua
+++ home/.config/awesome/handling.lua
@@ -5,9 +5,9 @@ local naughty = require("naughty")
 -- Check if awesome encountered an error during startup and fall back to
 -- another config (This code will only ever execute for the fallback config)
 naughty.connect_signal("request::display_error", function(message, startup)
-    naughty.notification {
-        urgency = "critical",
-        title   = "Oops, an error happened"..(startup and " during startup!" or "!"),
-        message = message
-    }
+  naughty.notification({
+    urgency = "critical",
+    title = "Oops, an error happened" .. (startup and " during startup!" or "!"),
+    message = message,
+  })
 end)
