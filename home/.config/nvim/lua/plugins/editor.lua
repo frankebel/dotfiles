2f0b5fa32aa8e853ef1c923c9109131b3865ae72
Frank Ebel <git@ebel.page>
2023-07-09 22:07:11 +0200

    feat(neovim): add plugin todo-comments

--- home/.config/nvim/lua/plugins/editor.lua
+++ home/.config/nvim/lua/plugins/editor.lua
@@ -58,4 +58,21 @@ return {
       view_options = { show_hidden = true },
     },
   },
+
+  -- highlight and search todo comments
+  -- https://github.com/folke/todo-comments.nvim
+  {
+    "folke/todo-comments.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    version = "*",
+    cmd = { "TodoTrouble", "TodoTelescope" },
+    event = { "BufReadPost", "BufNewFile" },
+    config = true,
+    -- stylua: ignore
+    keys = {
+      { "]t", function() require("todo-comments").jump_next() end, desc = "Next TODO" },
+      { "[t", function() require("todo-comments").jump_prev() end, desc = "Prev TODO" },
+      { "<leader>ft", "<cmd>TodoTelescope<cr>", desc = "Todo" },
+    },
+  },
 }
