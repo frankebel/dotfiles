7587dc4ed87b71732c9fd7f61052501e87610299
Frank Ebel <git@ebel.page>
2023-10-03 09:13:46 +0200

    feat(neovim): install Julia LSP automatically

--- home/.config/nvim/after/ftplugin/julia.lua
+++ home/.config/nvim/after/ftplugin/julia.lua
@@ -1,2 +1,10 @@
 -- Formatoptions
 vim.opt.formatoptions:remove({ "r", "o" })
+
+-- Install LSP automatically.
+local dir = vim.fn.expand("~/.julia/environments/nvim-lspconfig/")
+if vim.fn.isdirectory(dir) == 0 and vim.fn.executable("julia") == 1 then
+  os.execute(
+    "julia" .. " --project=~/.julia/environments/nvim-lspconfig" .. " -e 'using Pkg; Pkg.add(\"LanguageServer\")'"
+  )
+end
