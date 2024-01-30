6a768763ddd8608c5a37ff4c43b3ef403bbcfa3c
Frank Ebel <git@ebel.page>
2024-01-30 19:12:48 +0100

    refactor(wezterm): update fixed wayland

--- home/.config/wezterm/wezterm.lua
+++ home/.config/wezterm/wezterm.lua
@@ -13,7 +13,6 @@ return {
   color_scheme = "Dracula (Official)",
   disable_default_key_bindings = true,
   enable_tab_bar = false,
-  enable_wayland = false,
   font = wezterm.font_with_fallback({
     "FiraCode Nerd Font",
     "Fira Code",
