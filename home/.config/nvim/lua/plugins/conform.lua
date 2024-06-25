a018ffd7cd0c205451ad76a0ad8ae50c02df0c32
Frank Ebel <git@ebel.page>
2024-06-25 15:42:22 +0200

    chore(neovim): update conform.nvim 6.0.0

--- home/.config/nvim/lua/plugins/conform.lua
+++ home/.config/nvim/lua/plugins/conform.lua
@@ -11,7 +11,7 @@ return {
     {
       "<leader>cf",
       function()
-        require("conform").format({ async = true, lsp_fallback = true })
+        require("conform").format({ async = true, lsp_format = "fallback" })
       end,
       desc = "conform Format buffer",
     },
@@ -36,7 +36,7 @@ return {
     },
     format_on_save = {
       timeout_ms = 500,
-      lsp_fallback = true,
+      lsp_format = "fallback",
     },
   },
 }
