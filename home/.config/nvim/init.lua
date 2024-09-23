352a72cf155f545501dc53c9d2e6f71159d33dce
Frank Ebel <git@ebel.page>
2024-09-23 11:18:09 +0200

    fix(neovim): clone options before repo url

--- home/.config/nvim/init.lua
+++ home/.config/nvim/init.lua
@@ -11,8 +11,8 @@ if not vim.loop.fs_stat(lazypath) then
     "git",
     "clone",
     "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
     "--branch=stable", -- latest stable release
+    "https://github.com/folke/lazy.nvim.git",
     lazypath,
   })
 end
