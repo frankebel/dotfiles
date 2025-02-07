d4fc8535575db866612d09d44ab0e0bb11c4967d
Frank Ebel <git@ebel.page>
2025-02-07 11:20:39 +0100

    feat(btop): create config

--- scripts/config.sh
+++ scripts/config.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
+
+# qt5ct
 git config filter.ignore_geometry.smudge 'sed "s/geometry=.*/geometry={random string}/"'
 git config filter.ignore_geometry.clean 'sed "s/geometry=.*/geometry={random string}/"'
+
+# Ignore changes in the following files.
+# Track again with `git update-index --no-skip-worktree`.
+git update-index --skip-worktree home/.config/btop/btop.conf
