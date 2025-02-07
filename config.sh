3b041294ae665316bfd5b8a954d14460a1c5582c
Frank Ebel <git@ebel.page>
2025-02-07 12:35:19 +0100

    chore: move git config to root directory

---
+++ config.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+# Ignore changes in the following files.
+# Track again with `git update-index --no-skip-worktree`.
+git update-index --skip-worktree home/.config/btop/btop.conf
+git update-index --skip-worktree home/.config/qt5ct/qt5ct.conf
