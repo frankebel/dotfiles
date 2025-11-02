739b9f0d36ae790893065f43fc9f5fa17a5c9f7a
Frank Ebel <git@ebel.page>
2025-11-02 09:38:14 +0100

    fix(hyprland): startup program path

--- home/.config/hypr/device.sh
+++ home/.config/hypr/device.sh
@@ -5,7 +5,7 @@ case "$(hostnamectl hostname)" in
     *desktop*)
         hyprctl keyword monitor DP-1,preferred,2560x0,auto
         hyprctl keyword monitor DP-2,preferred,0x0,auto
-        ~/data/.environments/flexget/bin/flexget daemon start -d &
+        flexget daemon start -d &
         ;;
     *laptop*)
         hyprctl keyword input:kb_layout us,de
