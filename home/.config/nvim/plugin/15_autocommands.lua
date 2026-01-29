c79f4e8da3d0eb9e683426e3b9a2f421a377c3a0
Frank Ebel <git@ebel.page>
2026-01-29 13:42:32 +0100

    feat(neovim): create directories when saving files
    Taken from https://github.com/radleylewis/nvim-lite.

--- home/.config/nvim/plugin/15_autocommands.lua
+++ home/.config/nvim/plugin/15_autocommands.lua
@@ -13,3 +13,14 @@ vim.api.nvim_create_autocmd("BufReadPost", {
     end
   end,
 })
+
+-- Create directories when saving files
+vim.api.nvim_create_autocmd("BufWritePre", {
+  group = augroup,
+  callback = function()
+    local dir = vim.fn.expand("<afile>:p:h")
+    if vim.fn.isdirectory(dir) == 0 then
+      vim.fn.mkdir(dir, "p")
+    end
+  end,
+})
