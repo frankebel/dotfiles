a1e2244fc15ceb8bd80b834fdf0e11fa52231238
Frank Ebel <git@ebel.page>
2026-04-21 13:18:07 +0200

    refactor(neovim): move plugin config into its own file

--- home/.config/nvim/init.lua
+++ home/.config/nvim/init.lua
@@ -1,17 +1,2 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
-
--- Plugin to improve viewing Markdown files in Neovim
--- (needs to be loaded early)
-vim.pack.add({
-  {
-    src = "https://github.com/MeanderingProgrammer/render-markdown.nvim",
-    version = vim.version.range("*"),
-  },
-})
-
-require("render-markdown").setup({
-  sign = {
-    enabled = false,
-  },
-})
---
+++ home/.config/nvim/plugin/65_render-markdown.lua
@@ -0,0 +1,13 @@
+-- Plugin to improve viewing Markdown files in Neovim
+vim.pack.add({
+  {
+    src = "https://github.com/MeanderingProgrammer/render-markdown.nvim",
+    version = vim.version.range("*"),
+  },
+})
+
+require("render-markdown").setup({
+  sign = {
+    enabled = false,
+  },
+})
