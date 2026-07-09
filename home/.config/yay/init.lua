2d59bb4982a3657dc6acef3dc18a98c8aa2cb8a4
Frank Ebel <git@ebel.page>
2026-07-09 08:35:31 +0200

    refactor(yay): update config to v1.13.0
    It now uses Lua.

---
+++ home/.config/yay/init.lua
@@ -0,0 +1,8 @@
+-- Only track changes compared to default.
+
+yay.opt.answer_clean = "None"
+yay.opt.answer_diff = "All"
+yay.opt.bottom_up = false
+yay.opt.build_dir = os.getenv("XDG_CACHE_HOME") .. "/yay"
+yay.opt.clean_after = true
+yay.opt.sort_by = "votes"
