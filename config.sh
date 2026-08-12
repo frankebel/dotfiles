01b1483e4821c597302a75c4dfe024539bafd243
Frank Ebel <git@ebel.page>
2026-08-12 09:05:11 +0200

    refactor(qt): use Qt 6

--- config.sh
+++ config.sh
@@ -3,4 +3,4 @@
 # Ignore changes in the following files.
 # Track again with `git update-index --no-skip-worktree`.
 git update-index --skip-worktree home/.config/btop/btop.conf
-git update-index --skip-worktree home/.config/qt5ct/qt5ct.conf
+git update-index --skip-worktree home/.config/qt6ct/qt6ct.conf
