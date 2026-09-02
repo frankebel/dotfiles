f10953a999a373045687834d289a83d651490a43
Frank Ebel <git@ebel.page>
2026-09-02 14:53:51 +0200

    refactor(neovim): remove unused keymaps

--- home/.config/nvim/plugin/11_keymaps.lua
+++ home/.config/nvim/plugin/11_keymaps.lua
@@ -2,15 +2,6 @@ local function map(mode, lhs, rhs, desc)
   vim.keymap.set(mode, lhs, rhs, { desc = desc })
 end
 
-local function toggle_colorcolumn()
-  local value = vim.inspect(vim.opt.colorcolumn:get())
-  if value == "{}" then
-    vim.opt.colorcolumn = { "80", "100" }
-  else
-    vim.opt.colorcolumn = {}
-  end
-end
-
 -- Navigate buffers
 map("n", "<S-l>", "<cmd>bnext<cr>", "Next buffer")
 map("n", "<S-h>", "<cmd>bprevious<cr>", "Previous buffer")
@@ -27,26 +18,14 @@ map("n", "<C-Down>", "<cmd>resize -2<cr>", "Decrease window height")
 map("n", "<C-Left>", "<cmd>vertical resize -2<cr>", "Decrease window width")
 map("n", "<C-Right>", "<cmd>vertical resize +2<cr>", "Increase window width")
 
--- Open/close vsplit
-map("n", "<leader>-", "<cmd>vsplit<cr>", "Open vsplit")
-map("n", "<leader>_", "<c-w>q", "Close vsplit")
-
 -- Reselect visual area
 map("v", "<", "<gv", "Shift left")
 map("v", ">", ">gv", "Shift right")
 
 -- Search
 map({ "i", "n" }, "<esc>", "<cmd>nohlsearch<cr><esc>", "Escape and clear hlsearch")
-map({ "n", "x" }, "gw", "*N", "Search word under cursor")
-
--- Move text up and down
-map("x", "K", ":move '<-2<CR>gv-gv", "Move lines up")
-map("x", "J", ":move '>+1<CR>gv-gv", "Move lines down")
 
 -- Toggle options
-map("n", "<leader>oc", toggle_colorcolumn, "Toggle colorcolumn")
-map("n", "<leader>or", "<cmd>set invrelativenumber<cr>", "Toggle relativenumber")
-map("n", "<leader>os", "<cmd>set invspell<cr>", "Toggle spell")
 map("n", "<leader>ow", "<cmd>set invwrap<cr>", "Toggle wrap")
 
 -- Go line by line with `wrap` unless count (e.g. 4j) is given
